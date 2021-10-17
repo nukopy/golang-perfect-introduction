522542d339b236237f17c61bff36cd0d1b367fec
nukopy <pytwbf201830@gmail.com>
2021-10-17 19:14:15 +0900

    DONE chap 4.3

--- src/chap04/main.go
+++ src/chap04/main.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
-
-	"chap04/greeting"
-	"chap04/scope"
 )
+
+func init() {
+	// main の前に実行される
+	fmt.Println("init1")
+}
+
+func init() {
+	fmt.Println("init2")
+	main()
+	// main 関数は明示的に呼び出すことが出来る
+	// ここで呼び出した main とは別にまた main が呼ばれる。
+	// init はあくまでパッケージの初期化処理なので、
+	// ここで呼び出した main は初期化処理 1 つとして扱われる。
+}
 
 func main() {
 	fmt.Println("===== in chap04 =====")
+	fmt.Println("main")
 
 	// 4.1. パッケージ
 	// TRY パッケージを分けてみよう
-	greeting.Do()
+	// greeting.Do()
 	// greeting.do() unexported なメソッドなので外部からはアクセスできず、コンパイルエラーになる
 
 	// 4.2. パッケージ変数とスコープ
-	scope.CheckUniversalScope()
+	// scope.CheckUniversalScope()
 
 	// 4.3. パッケージの初期化
-	// todo
+	// init 関数の実行
 
 	// 4.4. ライブラリのバージョン管理
 	// todo
